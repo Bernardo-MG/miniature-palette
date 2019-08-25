e9c0af582561ee69469ef756a38857b640a5bb32
Bernardo <programming@wandrell.com>
2019-08-25 16:22:50 +0200

    Added comparison methods

--- src/main/java/com/bernardomg/tabletop/palette/paint/model/PaintOption.java
+++ src/main/java/com/bernardomg/tabletop/palette/paint/model/PaintOption.java
@@ -4,6 +4,9 @@ package com.bernardomg.tabletop.palette.paint.model;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.io.Serializable;
+import java.util.Objects;
+
+import com.google.common.base.MoreObjects;
 
 public class PaintOption implements Serializable {
 
@@ -15,12 +18,40 @@ public class PaintOption implements Serializable {
         super();
     }
 
+    @Override
+    public final boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final PaintOption other = (PaintOption) obj;
+        return Objects.equals(name, other.name);
+    }
+
     public String getName() {
         return name;
     }
 
+    @Override
+    public final int hashCode() {
+        return Objects.hash(name);
+    }
+
     public void setName(final String value) {
         name = checkNotNull(value, "Received a null pointer as name");
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("name", name).toString();
     }
 
 }
