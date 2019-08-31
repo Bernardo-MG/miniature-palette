30f07d1ef4f1bea7498c149c59c93e34ec2d2f38
Bernardo <programming@wandrell.com>
2019-09-01 00:52:02 +0200

    The local state supports multiple palettes

--- src/main/js/views/components/PaletteEditor.js
+++ src/main/js/views/components/PaletteEditor.js
@@ -7,23 +7,40 @@ import Palette from 'paints/components/Palette';
 
 function PaletteEditor() {
 
-   const [palettes, setPalettes] = useState([]);
+   const [palettes, setPalettes] = useState([{ colors: [] }]);
+
+   // const [paletteIndex, setPaletteIndex] = useState(-1);
 
    const [color, setColor] = useState('');
 
-   function addPalette() {
-      setPalettes([color, ...palettes]);
+   // function addPalette() {
+   //    const original = palettes[index];
+   //    const palette = { name: 'palette'+paletteIndex, colors: [] };
+   //    setPalettes([...palettes, palette]);
+   //    setPaletteIndex(paletteIndex + 1);
+   // }
+
+   function addColorToCurrent() {
+      const index = 0;
+      const newPalette = palettes[0];
+      newPalette.colors = [color, ...newPalette.colors];
+      // const palette = { name: 'name', ...original, colors: [color, ...original.colors] };
+      const newPalettes = [...palettes];
+      newPalettes[index] = newPalette;
+      setPalettes(newPalettes);
    }
 
    return <React.Fragment>
       <div>
          <PaintSuggestionInput
             onChoose={setColor} />
-         <Button onClick={addPalette}>
+         <Button onClick={addColorToCurrent}>
             { 'add' }
          </Button>
       </div>
-      <Palette palette={palettes}/>
+      {palettes.map((palette) =>
+         <Palette key={palette.name} palette={palette.colors}/>
+      )}
    </React.Fragment>;
 }
 
