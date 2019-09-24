d8adfcc547d49bd150e7ff80b81ae07c9bd67710
Bernardo <programming@wandrell.com>
2019-09-24 19:57:00 +0200

    Fixed test script

--- src/test/resources/db/paints.sql
+++ src/test/resources/db/paints.sql
@@ -1,6 +1,6 @@
-INSERT INTO products (name, code) VALUES
-   ('White', '70.951'),
-   ('Foundation White', '70.919'),
-   ('Gloss White', '70.842'),
-   ('Offwhite', '70.820'),
-   ('Ivory', '70.918');
+INSERT INTO products (id, name, code) VALUES
+   (1, 'White', '70.951'),
+   (2, 'Foundation White', '70.919'),
+   (3, 'Gloss White', '70.842'),
+   (4, 'Offwhite', '70.820'),
+   (5, 'Ivory', '70.918');
