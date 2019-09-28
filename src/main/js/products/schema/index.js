00bab3e1966f2eb3007ede267b207a3fc77842c4
Bernardo <programming@wandrell.com>
2019-09-28 16:57:31 +0200

    Using composite id

--- src/main/js/products/schema/index.js
+++ src/main/js/products/schema/index.js
@@ -4,5 +4,5 @@ import { schema } from 'normalizr';
  * Document schema.
  */
 export const product = new schema.Entity('products', {}, {
-   idAttribute: 'code'
+   idAttribute: (value) => `${value.code}-${value.brand}-${value.company}`.replace(/ /g, '_')
 });
