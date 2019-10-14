ac71546a581e4142dc83bb8bb7e5f08c82fd1629
Bernardo <programming@wandrell.com>
2019-10-14 20:55:31 +0200

    Corrections

--- src/main/js/palettes/components/PaletteGroupList.js
+++ src/main/js/palettes/components/PaletteGroupList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 
@@ -29,7 +29,8 @@ function PaletteGroupList({ load, palettes }) {
 }
 
 PaletteGroupList.propTypes = {
-   load: PropTypes.func.isRequired
+   load: PropTypes.func.isRequired,
+   palettes: PropTypes.array.isRequired
 };
 
 const mapStateToProps = (state) => {
