216611f6c9493bf07e3e0cb0140b24cdb5fa5163
Bernardo <programming@wandrell.com>
2019-10-20 15:35:00 +0200

    Moved hook to its own file

--- src/main/js/palettes/components/PaletteEditor.js
+++ src/main/js/palettes/components/PaletteEditor.js
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import PropTypes from 'prop-types';
-
-import { useDispatch, useSelector } from 'react-redux';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -10,27 +8,7 @@ import CardContent from '@material-ui/core/CardContent';
 import PaletteEditorList from 'palettes/components/PaletteEditorList';
 import PaintInput from 'palettes/components/PaintInput';
 
-import { selectSuggestions, selectLoaded } from 'products/selectors';
-
-import { read, setLoaded } from 'products/actions';
-
-function useSuggestions() {
-   const suggestions = useSelector(selectSuggestions);
-   const loaded = useSelector(selectLoaded);
-
-   const dispatch = useDispatch();
-   const load = () => dispatch(read());
-   const setLoad = (input) => dispatch(setLoaded(input));
-
-   useEffect(() => {
-      if (!loaded) {
-         setLoad(true);
-         load();
-      }
-   });
-
-   return suggestions;
-}
+import { useSuggestions } from 'suggestions';
 
 function PaletteEditor({ palette, addPalette }) {
 
