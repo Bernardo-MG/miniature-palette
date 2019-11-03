6f0731966b8946496e5e26ca0b47f64595e2f2d4
Bernardo <programming@wandrell.com>
2019-11-03 23:30:47 +0100

    Corrected reducer

--- src/main/js/notifications/reducers/index.js
+++ src/main/js/notifications/reducers/index.js
@@ -1,7 +1,7 @@
 import * as types from 'notifications/actions/types';
 
 const defaultState = {
-   notifications: {}
+   notifications: []
 };
 
 const notification = (state = defaultState, action) => {
