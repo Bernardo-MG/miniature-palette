ad433042eef204074dc1ec8da00d43d74db28e5f
Bernardo <programming@wandrell.com>
2019-11-09 15:16:13 +0100

    Corrected validation

--- src/main/java/com/bernardomg/tabletop/palette/palette/model/PaintOption.java
+++ src/main/java/com/bernardomg/tabletop/palette/palette/model/PaintOption.java
@@ -21,9 +21,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.google.common.base.MoreObjects;
 
@@ -32,8 +31,7 @@ public class PaintOption implements Serializable {
     private static final long serialVersionUID = -7040753038901687866L;
 
     @NotNull
-    @Min(1)
-    @Max(50)
+    @Size(min = 1, max = 50)
     private String            name             = "";
 
     public PaintOption() {
--- src/main/java/com/bernardomg/tabletop/palette/palette/model/PaletteGroupOption.java
+++ src/main/java/com/bernardomg/tabletop/palette/palette/model/PaletteGroupOption.java
@@ -19,17 +19,15 @@ package com.bernardomg.tabletop.palette.palette.model;
 import java.util.ArrayList;
 import java.util.Objects;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.google.common.base.MoreObjects;
 
 public class PaletteGroupOption {
 
     @NotNull
-    @Min(1)
-    @Max(50)
+    @Size(min = 1, max = 50)
     private String                  name;
 
     private Iterable<PaletteOption> palettes = new ArrayList<>();
