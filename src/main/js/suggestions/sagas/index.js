f683adfc7be18a7973f1f970ee6119a723c932b0
Bernardo <programming@wandrell.com>
2019-12-09 17:50:57 +0100

    Suggestions are stored correctly

--- src/main/js/suggestions/sagas/index.js
+++ src/main/js/suggestions/sagas/index.js
@@ -6,10 +6,16 @@ import { READ_PRODUCTS_SUCCESS } from 'products/actions/types';
 
 import { setPaintSuggestions } from 'suggestions/actions';
 
+function toMap(value) {
+   return { value: `${value.name} (${value.code})`, label: `${value.name} (${value.code})` };
+}
+
 export function* storePaintSuggestions(action) {
    const normalized = normalize(action.payload, [product]);
    if (normalized.entities.products) {
-      yield put(setPaintSuggestions(normalized.entities.products));
+      const values = Object.values(normalized.entities.products);
+      const suggestions = values.map(toMap);
+      yield put(setPaintSuggestions(suggestions));
    }
 }
 
--- src/main/js/suggestions/selectors/index.js
+++ src/main/js/suggestions/selectors/index.js
@@ -1,6 +1,4 @@
 
-export const selectPaints = (state) => state.suggestion.paints;
-
-export const selectSuggestions = (state) => Object.values(selectPaints(state)).map((product) => `${product.name} (${product.code})`);
+export const selectSuggestions = (state) => state.suggestion.paints;
 
 export const selectLoaded = (state) => state.suggestion.loadedPaints;
