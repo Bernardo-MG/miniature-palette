7644eed0f5eb2e11476b7ec533479305f2b9ec01
Bernardo <programming@wandrell.com>
2019-12-23 22:12:52 +0100

    Added tests

--- src/test/java/com/bernardomg/tabletop/palette/test/integration/palette/service/ITPaletteServiceUpdateReturn.java
+++ src/test/java/com/bernardomg/tabletop/palette/test/integration/palette/service/ITPaletteServiceUpdateReturn.java
@@ -16,8 +16,26 @@
 
 package com.bernardomg.tabletop.palette.test.integration.palette.service;
 
-import org.junit.jupiter.api.Disabled;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bernardomg.tabletop.palette.palette.model.data.PaintData;
+import com.bernardomg.tabletop.palette.palette.model.data.PaletteData;
+import com.bernardomg.tabletop.palette.palette.model.form.PaintForm;
+import com.bernardomg.tabletop.palette.palette.model.form.PaletteUpdateForm;
+import com.bernardomg.tabletop.palette.palette.service.PaletteService;
+import com.google.common.collect.Iterables;
 
 /**
  * Integration tests for the {@link ExampleEntityService}.
@@ -26,16 +44,106 @@ import org.springframework.test.context.jdbc.Sql;
  * the example entities repository, these tests are for verifying everything is
  * set up correctly and working.
  */
+@RunWith(JUnitPlatform.class)
+@SpringJUnitConfig
+@Transactional
+@Rollback
+@ContextConfiguration(
+        locations = { "classpath:context/application-context.xml" })
 @Sql({ "/db/palette_simple.sql", "/db/paint_simple.sql" })
-@Disabled
-public class ITPaletteServiceUpdateReturn
-        extends AbstractITPaletteServiceReturn {
+public class ITPaletteServiceUpdateReturn {
+
+    /**
+     * Service being tested.
+     */
+    @Autowired
+    private PaletteService service;
 
     /**
      * Default constructor.
      */
     public ITPaletteServiceUpdateReturn() {
         super();
+    }
+
+    @Test
+    public void testUpdatePalette_Empty() {
+        final PaletteUpdateForm palette;
+        final PaletteData result;
+
+        palette = new PaletteUpdateForm();
+
+        result = service.updatePalette(palette);
+
+        Assertions.assertNull(result);
+    }
+
+    @Test
+    public void testUpdatePalette() {
+        final PaletteUpdateForm palette;
+        final PaletteData result;
+
+        palette = new PaletteUpdateForm();
+        palette.setId(1l);
+        palette.setName("palette");
+
+        result = service.updatePalette(palette);
+
+        Assertions.assertEquals(1l, result.getId());
+        Assertions.assertEquals("palette", result.getName());
+    }
+
+    @Test
+    public void testUpdatePalette_Paints_ReturnsPaintData() {
+        final PaletteUpdateForm palette;
+        final Collection<PaintForm> paints;
+        final PaintForm paint;
+        final PaletteData result;
+        final PaintData resultPaint;
+
+        paint = new PaintForm();
+        paint.setName("paint");
+
+        paints = new ArrayList<>();
+        paints.add(paint);
+
+        palette = new PaletteUpdateForm();
+        palette.setId(1l);
+        palette.setName("palette");
+        palette.setPaints(paints);
+
+        result = service.updatePalette(palette);
+
+        Assertions.assertEquals(1, Iterables.size(result.getPaints()));
+
+        resultPaint = result.getPaints().iterator().next();
+
+        Assertions.assertEquals(1l, resultPaint.getId());
+        Assertions.assertEquals("paint", resultPaint.getName());
+    }
+
+    @Test
+    public void testUpdatePalette_Paints_ReturnsPaletteData() {
+        final PaletteUpdateForm palette;
+        final Collection<PaintForm> paints;
+        final PaintForm paint;
+        final PaletteData result;
+
+        paint = new PaintForm();
+        paint.setName("paint");
+
+        paints = new ArrayList<>();
+        paints.add(paint);
+
+        palette = new PaletteUpdateForm();
+        palette.setId(1l);
+        palette.setName("palette");
+        palette.setPaints(paints);
+
+        result = service.updatePalette(palette);
+
+        Assertions.assertEquals(1l, result.getId());
+        Assertions.assertEquals("palette", result.getName());
     }
 
 }
