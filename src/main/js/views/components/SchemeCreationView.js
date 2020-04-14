6cb06ea664c09b4cdc034653c368857ccf185dae
Bernardo <programming@wandrell.com>
2020-04-14 21:00:56 +0200

    Defined new layout

--- src/main/js/views/components/SchemeCreationView.js
+++ src/main/js/views/components/SchemeCreationView.js
@@ -3,12 +3,23 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import SaveIcon from '@material-ui/icons/Save';
+
+import SchemeEditor from 'palettes/components/SchemeEditor';
 
 import { useDispatch } from 'react-redux';
 
 import { saveScheme } from 'api/actions';
-
-import SchemeEditor from 'palettes/components/SchemeEditor';
 
 import { usePalettes } from 'domain/hooks';
 
@@ -34,11 +45,45 @@ function SchemeCreationView() {
       dispatch(saveScheme(form));
    }
 
-   return <Box className={classes.root}>
-      <SchemeEditor
-         palettes={palettes}
-         onSave={handleSave}
-         initialValues={values} />
+   return <Box className={classes.root} width={1}>
+      <Grid container>
+         <Grid item xs={1}>
+            <MenuList>
+               <MenuItem>
+                  <ListItemIcon>
+                     <SaveIcon />
+                  </ListItemIcon>
+               </MenuItem>
+            </MenuList>
+         </ Grid>
+         <Grid item xs={9}>
+            <SchemeEditor
+               palettes={palettes}
+               onSave={handleSave}
+               initialValues={values} />
+         </ Grid>
+         <Grid item xs={2}>
+            <List>
+               <ListItem button>
+                  <ListItemText primary="Palette 1" />
+               </ListItem>
+               <ListItem button>
+                  <ListItemText primary="Palette 2" />
+               </ListItem>
+               <ListItem button>
+                  <ListItemText primary="Palette 3" />
+               </ListItem>
+            </List>
+            <Divider />
+            <List>
+               <ListItem button>
+                  <ListItemIcon>
+                     <AddCircleIcon />
+                  </ListItemIcon>
+               </ListItem>
+            </List>
+         </ Grid>
+      </ Grid>
    </Box>;
 }
 
