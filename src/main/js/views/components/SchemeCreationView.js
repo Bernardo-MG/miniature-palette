0f3d67a94a92fbfa3ee6b48bae4d5aa724751370
Bernardo <programming@wandrell.com>
2020-04-17 23:43:33 +0200

    Moved palette form to modal

--- src/main/js/views/components/SchemeCreationView.js
+++ src/main/js/views/components/SchemeCreationView.js
@@ -6,8 +6,12 @@ import * as Yup from 'yup';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -18,10 +22,9 @@ import MenuList from '@material-ui/core/MenuList';
 import TextField from '@material-ui/core/TextField';
 
 import AddIcon from '@material-ui/icons/Add';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
 import SaveIcon from '@material-ui/icons/Save';
 
-import PaletteCreateForm from 'palettes/containers/PaletteCreateForm';
+import PaintsEditorList from 'palettes/components/PaintsEditorList';
 
 import { Formik, Form, FieldArray } from 'formik';
 
@@ -38,6 +41,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const SchemeSchema = Yup.object().shape({
+   name: Yup.string()
+      .min(0, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required')
+});
+
+const PaletteSchema = Yup.object().shape({
    name: Yup.string()
       .min(0, 'Too Short!')
       .max(50, 'Too Long!')
@@ -61,8 +71,71 @@ PaletteSelectionList.propTypes = {
    onSelect: PropTypes.func
 };
 
+function PaletteCreationDialog({ open, onClose, onSelect }) {
+   const initialValues = {
+      name: '',
+      paints: []
+   };
+
+   return <Formik
+      onSubmit={(form) => onSelect(form)}
+      initialValues={initialValues}
+      validationSchema={PaletteSchema}>
+      {({ values, errors, touched, handleChange, handleBlur }) => (
+         <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">add_palette</DialogTitle>
+            <DialogContent>
+               <Form>
+                  <Grid container spacing={3}>
+                     <Grid item xs={12}>
+                        <Box m={2}>
+                           <TextField
+                              fullWidth
+                              name="name"
+                              label="palette_name"
+                              value={values.name}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              helperText={(errors.name && touched.name) && errors.name}
+                              margin="normal"
+                           />
+                        </Box>
+                     </Grid>
+                     <FieldArray
+                        name="paints"
+                        render={(arrayHelpers) => (
+                           <PaintsEditorList data={values.paints}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              errors={errors}
+                              touched={touched}
+                              onAdd={() => arrayHelpers.push({ name: '' })}
+                              onRemove={(index) => arrayHelpers.remove(index)} />
+                        )}
+                     />
+                  </Grid>
+               </Form>
+            </DialogContent>
+            <DialogActions>
+               <Button onClick={onClose} color="primary">
+                  cancel
+               </Button>
+               <Button onClick={onSelect} color="primary">
+                  add
+               </Button>
+            </DialogActions>
+         </Dialog>
+      )}
+   </Formik>;
+}
+
+PaletteCreationDialog.propTypes = {
+   open: PropTypes.boolean,
+   onClose: PropTypes.func,
+   onSelect: PropTypes.func
+};
+
 function SchemeCreationView() {
-   const [selecting, setSelecting] = useState(false);
    const [creating, setCreating] = useState(false);
 
    const classes = useStyles();
@@ -78,98 +151,75 @@ function SchemeCreationView() {
       dispatch(saveScheme(form));
    }
 
-   function handleSelect(values, data) {
-      setSelecting(false);
-      values.push(data);
-   }
-
-   function handleFinishPaletteCreation() {
+   function handleFinishPaletteCreation(values, data) {
       setCreating(false);
+      values.push(data);
    }
 
    function handleShowPaletteCreation() {
       setCreating(true);
    }
 
-   function handleShowPaletteSelection() {
-      setSelecting(true);
-   }
-
-   let component;
-
-   if (creating) {
-      component = <PaletteCreateForm onReturn={handleFinishPaletteCreation} />;
-   } else {
-      component = <Box className={classes.root} width={1}>
-         <Formik onSubmit={handleSave} initialValues={initialValues} validationSchema={SchemeSchema}>
-            {({ values, errors, touched, handleChange, handleBlur }) => (
-               <Form>
-                  <Grid container>
-                     <Grid item xs={2}>
-                        <MenuList>
-                           <MenuItem>
-                              <ListItemIcon>
-                                 <SaveIcon />
-                              </ListItemIcon>
-                           </MenuItem>
-                        </MenuList>
-                     </ Grid>
-                     <Grid item xs={8}>
-                        <Grid item xs={12}>
-                           <Box m={2}>
-                              <TextField
-                                 fullWidth
-                                 name="name"
-                                 label="scheme_name"
-                                 value={values.name}
-                                 onChange={handleChange}
-                                 onBlur={handleBlur}
-                                 helperText={(errors.name && touched.name) && errors.name}
-                                 margin="normal"
-                              />
-                           </Box>
-                        </Grid>
-                     </ Grid>
-                     <Grid item xs={2}>
-                        <FieldArray
-                           name="palettes"
-                           render={(arrayHelpers) => (
-                              <Fragment>
-                                 <List>
-                                    {values.palettes.map((palette) =>
-                                       <ListItem button key={palette.name}>
-                                          <ListItemText primary={palette.name}/>
-                                       </ListItem>
-                                    )}
-                                 </List>
-                                 <Divider />
-                                 <List>
-                                    <ListItem button aria-label="create" onClick={handleShowPaletteCreation}>
-                                       <ListItemIcon>
-                                          <AddIcon />
-                                       </ListItemIcon>
-                                    </ListItem>
-                                    <ListItem button aria-label="copy" onClick={handleShowPaletteSelection}>
-                                       <ListItemIcon>
-                                          <FileCopyIcon />
-                                       </ListItemIcon>
-                                    </ListItem>
-                                 </List>
-                                 <Drawer open={selecting} onClose={() => setSelecting(false)}>
-                                    <PaletteSelectionList onSelect={(palette) => handleSelect(arrayHelpers, palette)} />
-                                 </Drawer>
-                              </Fragment>
-                           )}
-                        />
-                     </ Grid>
+   return <Box className={classes.root} width={1}>
+      <Formik onSubmit={handleSave} initialValues={initialValues} validationSchema={SchemeSchema}>
+         {({ values, errors, touched, handleChange, handleBlur }) => (
+            <Form>
+               <Grid container>
+                  <Grid item xs={2}>
+                     <MenuList>
+                        <MenuItem>
+                           <ListItemIcon>
+                              <SaveIcon />
+                           </ListItemIcon>
+                        </MenuItem>
+                     </MenuList>
                   </ Grid>
-               </Form>
-            )}
-         </Formik>
-      </Box>;
-   }
-
-   return component;
+                  <Grid item xs={8}>
+                     <Grid item xs={12}>
+                        <Box m={2}>
+                           <TextField
+                              fullWidth
+                              name="name"
+                              label="scheme_name"
+                              value={values.name}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              helperText={(errors.name && touched.name) && errors.name}
+                              margin="normal"
+                           />
+                        </Box>
+                     </Grid>
+                  </ Grid>
+                  <Grid item xs={2}>
+                     <FieldArray
+                        name="palettes"
+                        render={(arrayHelpers) => (
+                           <Fragment>
+                              <List>
+                                 {values.palettes.map((palette) =>
+                                    <ListItem button key={palette.name}>
+                                       <ListItemText primary={palette.name}/>
+                                    </ListItem>
+                                 )}
+                              </List>
+                              <Divider />
+                              <List>
+                                 <ListItem button aria-label="create" onClick={handleShowPaletteCreation}>
+                                    <ListItemIcon>
+                                       <AddIcon />
+                                    </ListItemIcon>
+                                 </ListItem>
+                              </List>
+                              <PaletteCreationDialog open={creating} onClose={() => setCreating(false)} onSelect={(palette) => handleFinishPaletteCreation(arrayHelpers, palette)} />
+                           </Fragment>
+                        )}
+                     />
+                  </ Grid>
+               </ Grid>
+            </Form>
+         )}
+      </Formik>
+   </Box>;
 }
 
 SchemeCreationView.propTypes = {};
