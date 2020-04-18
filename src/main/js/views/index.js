f44785c9dd026de79da4fabacf337fbac262a12e
Bernardo <programming@wandrell.com>
2020-04-18 16:01:25 +0200

    Removed palette edition components

--- src/main/js/views/index.js
+++ src/main/js/views/index.js
@@ -1,7 +1,5 @@
 import Index from 'views/components/Index';
-import PaletteCreationView from 'views/components/PaletteCreationView';
-import PaletteListView from 'views/components/PaletteListView';
 import SchemeCreationView from 'views/components/SchemeCreationView';
 import SchemeListView from 'views/components/SchemeListView';
 
-export { Index, PaletteCreationView, PaletteListView, SchemeCreationView, SchemeListView };
+export { Index, SchemeCreationView, SchemeListView };
