8516d34c28b2ab32927a2c444319e877626ae329
Bernardo <programming@wandrell.com>
2020-04-20 20:22:18 +0200

    Moved component to view

--- src/main/js/views/components/SchemeListView.js
+++ src/main/js/views/components/SchemeListView.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 
+import { useDispatch } from 'react-redux';
+
+import { useSchemes } from 'domain/hooks';
+
 import Box from '@material-ui/core/Box';
 
-import SchemeListing from 'palettes/containers/SchemeListing';
+import SchemeList from 'palettes/components/SchemeList';
+
+import { deleteScheme, updateScheme, paletteReport } from 'api/actions';
 
 const useStyles = makeStyles((theme) => ({
    root: {
@@ -13,11 +19,29 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function SchemeListView() {
-
    const classes = useStyles();
 
+   const palettes = useSchemes();
+
+   const dispatch = useDispatch();
+
+   function handleReport(id) {
+      dispatch(paletteReport(id));
+   }
+
+   function handleSave(form) {
+      dispatch(updateScheme(form));
+   }
+
+   function handleDelete(form) {
+      dispatch(deleteScheme(form.id));
+   }
+
    return <Box className={classes.root}>
-      <SchemeListing />
+      <SchemeList data={palettes}
+         onReport={handleReport}
+         onSave={handleSave}
+         onDelete={handleDelete} />
    </Box>;
 }
 
