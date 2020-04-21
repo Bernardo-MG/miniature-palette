17d242cf1a9cb8723620586f0044934983d09c59
Bernardo <programming@wandrell.com>
2020-04-21 19:35:39 +0200

    Removed dead code

--- src/main/js/notifications/sagas/index.js
+++ src/main/js/notifications/sagas/index.js
@@ -1,7 +1,5 @@
-import { paletteNotificationSagas } from 'notifications/sagas/palette';
 import { schemeNotificationSagas } from 'notifications/sagas/scheme';
 
 export const notificationSagas = [
-   ...paletteNotificationSagas,
    ...schemeNotificationSagas
 ];
